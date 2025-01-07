a4d1beb132f13beea053aad966deb8127007fdab
Jesus <jesusmaquera10@gmail.com>
2025-01-07 14:57:44 -0500

    User resources

---
+++ app/Filament/Resources/UserResource/RelationManagers/SeriesRelationManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SeriesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'series';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+
+                Tables\Actions\ViewAction::make()
+                    ->url(fn ($record) => route('filament.admin.resources.series.edit', ['record' => $record])),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
